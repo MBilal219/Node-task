71c891ab589f29be427e5b0719740c6b68f5ebd0
MBilal219 <mbilal@gtek.solutions>
2024-02-03 18:13:55 +0500

    getting files from a directory and download content is completed

--- index.js
+++ index.js
@@ -3,14 +3,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { FetchApiData } = require("./utils/nodeUtils");
+const {
+  FetchApiData,
+  DownloadContentsFromURLs,
+  GetFilesFromFolder,
+} = require("./utils/nodeUtils");
+
+const ArryOfUrls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/posts/3",
+];
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-//connect to mongodb
+//mongodb connection
+
 const URI = process.env.MONGODB_URL;
 mongoose.connect(
   URI,
@@ -23,14 +34,40 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
 // fetch api task to check please uncomment blow function and pass any fetch api url
 
 // FetchApiData("https://jsonplaceholder.typicode.com/posts")
 //   .then((res) => console.log(res, "res"))
 //   .catch((err) => console.log(err, "err"));
 
+
+
+// download content in .txt format
+
+// DownloadContentsFromURLs(ArryOfUrls)
+//   .then((downloadedContents) => {
+//     console.log("Downloaded Contents:", downloadedContents);
+//   })
+//   .catch((err) => {
+//     console.error("err", err);
+//   });
+
+
+
+// get files on the base of extentions
+
+// const folderPath = "C:/Users/Muhammad Bilal/Downloads/Documents"; // Replace with your actual directory path
+// const extention = "pdf";
+
+// GetFilesFromFolder(folderPath, extention)
+// .then((files)=>console.log(`Files with extension .${extention}:`, files))
+// .catch((err)=>console.log(err,'<----err'));
+  
+
 //APIs
 app.use("/users", require("./routes/userRouter"));
+
 app.get("/", (req, res) => {
   res.json({ msg: "main api is working" });
 });
--- utils/nodeUtils.js
+++ utils/nodeUtils.js
@@ -1,3 +1,6 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 const nodeUtils = {
   FetchApiData: async (fetchEndPoint) => {
     try {
@@ -14,6 +17,55 @@ const nodeUtils = {
     } catch (err) {
       console.log("err--->", err);
       throw new Error(err);
+    }
+  },
+  // download content
+  DownloadContentsFromURLs: async (urls) => {
+    try {
+      if (!Array.isArray(urls))
+        throw new Error("Please provide an array of urls");
+
+      const downloadPromises = urls.map(async (url, i) => {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(
+            `Failed to download content from ${url}. Status: ${response.status}`
+          );
+        }
+
+        const content = await response.text();
+        const filename = `downloaded_content_${i + 1}.txt`;
+        await fs.writeFile(filename, content);
+
+        return filename;
+      });
+
+      const downloadedContents = await Promise.all(downloadPromises);
+
+      return downloadedContents;
+    } catch (error) {
+      console.error("Download failed:", error.message);
+      throw error;
+    }
+  },
+
+  GetFilesFromFolder: async (folderPath, extension) => {
+    try {
+      if (!folderPath || !typeof folderPath === "string") {
+        console.log("Please neter valid path");
+        throw new Error("Please enter valid path");
+      }
+      const files = await fs.readdir(folderPath);
+      const filteredFiles = files.filter((file) => {
+        const fileExtension = path.extname(file);
+        return fileExtension === `.${extension}`;
+      });
+
+      return filteredFiles;
+    } catch (error) {
+      console.error("Error:", error.message);
+      throw error;
     }
   },
 };
